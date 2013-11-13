ca0d6ae943ef43ace001fe53f6542d3d27d775d6
Greg Nicholas <greg.nicholas@gmail.com>
2013-11-13 16:23:57 -0500

    fix bottom constraint in about view

---
+++ MWCDCApp/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  MWCDCApp
+//
+//  Created by Greg Nicholas on 11/13/13.
+//  Copyright (c) 2013 Scenable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomLayoutSpaceConstraint;
+
+@end
---
+++ MWCDCApp/AboutViewController.m
@@ -0,0 +1,49 @@
+//
+//  AboutViewController.m
+//  MWCDCApp
+//
+//  Created by Greg Nicholas on 11/13/13.
+//  Copyright (c) 2013 Scenable. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidLayoutSubviews
+{
+    // fix for potential bug in iOS 7 where the bottomLayoutGuide in a UITabBarController
+    // changes from 49 to 0 after the first tab is chosen
+    if (self.bottomLayoutGuide.length == 0.0) {
+        // 49+12 = 61. no contentView here, so we need a 12pt buffer at the bottom
+        _bottomLayoutSpaceConstraint.constant = 61;
+    }
+    [self.view layoutSubviews];
+}
+
+@end
